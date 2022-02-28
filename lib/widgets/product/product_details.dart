48793a46a3cbd4172267f38dd539ab734f0c88a9
riad_zakir@yahoo.com <riad_zakir@yahoo.com>
2022-02-28 22:41:14 +0600

    Product details added

--- lib/widgets/product/product_details.dart
+++ lib/widgets/product/product_details.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommercify/providers/products_provider.dart';
 import 'package:provider/provider.dart';
@@ -14,6 +16,40 @@ class ProductDetails extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: 300,
+            child: Image.network(
+              loadedProduct.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            '\$${loadedProduct.price}',
+            style: const TextStyle(
+              color: Colors.grey,
+              fontSize: 20,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            width: double.infinity,
+            child: Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            ),
+          )
+        ]),
       ),
     );
   }
